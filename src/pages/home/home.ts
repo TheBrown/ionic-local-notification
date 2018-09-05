364a7e3734e6497ecf826efaab379d6d3f92fcef
Saleumsack <saleumsack16@gmail.com>
2018-09-05 10:08:06 +0700

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+import { LocalNotifications } from "@ionic-native/local-notifications";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private localNotifications: LocalNotifications
+  ) {
+    this.scheduleNotification();
 
   }
 
+  scheduleNotification() {
+    this.localNotifications.schedule({
+      text: "Delay localNotification",
+      trigger: { at: new Date(new Date().getTime() + 3600) },
+      led: "FF0000",
+      sound: null
+    });
+  }
 }
